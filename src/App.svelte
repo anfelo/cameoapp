6a54f04220dfaa923abc09b8b8dd8658a37e300c
Andres Osorio <af.osorio1341@gmail.com>
2021-03-25 12:36:45 -0500

    Initial commit

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,76 @@
 <script>
-	export let name;
-</script>
+	import { onMount } from 'svelte';
+	import { select } from './select';
+	import { loadImage } from './utils';
+	import Welcome from './screens/Welcome.svelte';
+	import Game from './screens/Game.svelte';
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
+	let state = 'welcome';
+	let celebsPromise;
+	let selection;
+
+	const start = async(event) => {
+		const { celebs, lookup } = await celebsPromise;
+
+		selection = select(celebs, lookup, event.detail.category);
+		state = 'playing';
+	};
+
+	const loadCelebs = async () => {
+		const res = await fetch('https://cameo-explorer.netlify.app/celebs.json');
+		const data = await res.json();
+
+		const lookup = new Map();
+		data.forEach(celeb => {
+			lookup.set(celeb.id, celeb);
+		});
+		
+		const subset = new Set();
+		data.forEach(celeb => {
+			if (celeb.reviews >= 50) {
+				subset.add(celeb);
+				celeb.similar.forEach(id => {
+					subset.add(lookup.get(id));
+				});
+			}
+		});
+
+		return {
+			celebs: Array.from(subset),
+			lookup
+		}
+	};
+
+	onMount(() => {
+		celebsPromise = loadCelebs();
+
+		loadImage('/icons/right.svg');
+		loadImage('/icons/wrong.svg');
+	});
+</script>
 
 <style>
 	main {
 		text-align: center;
 		padding: 1em;
-		max-width: 240px;
+		max-width: 800px;
 		margin: 0 auto;
+		height: 100%;
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
 	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
 	@media (min-width: 640px) {
 		main {
 			max-width: none;
 		}
 	}
 </style>
+
+<main>
+	{#if state === 'welcome'}
+		<Welcome on:select={start} />
+	{:else if state === 'playing'}
+		<Game rounds={selection} on:restart={() => state = 'welcome'} />
+	{/if}
+</main>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,11 @@
 import App from './App.svelte';
 
 const app = new App({
-	target: document.body,
-	props: {
-		name: 'world'
-	}
+	target: document.body
 });
+
+if ('serviceWorker' in navigator) {
+	navigator.serviceWorker.register('/service-worker.js');
+}
 
 export default app;
